c8b6371f221b4a0aa045e174f0e6ef4b80f719f4
muahand-neyma <losylos084@gamil.com>
2024-08-07 17:18:56 -0700

    tyt

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 const server = http.createServer(app);
 
 const defaultValue = ""
-
+const PORT = 4000
 
 
 mongoose.connect("mongodb+srv://j4116507:0JWcQEPTfu0yxQxP@cluster0.nfqnxbb.mongodb.net/")
